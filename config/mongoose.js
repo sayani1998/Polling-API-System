4217ed38e627ef677817eb5a62dd854760a22735
Sayani Manna <113409943+sayani1998@users.noreply.github.com>
2023-03-13 14:26:08 +0530

    Update mongoose.js

--- config/mongoose.js
+++ config/mongoose.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose');
 
 //connect to the database
 // mongoose.connect('mongodb://localhost/contacts_list_db');
-mongoose.connect('mongodb://127.0.0.1/api-polling');
+mongoose.connect('mongodb+srv://Sayani7602:WtCdZ7i1r1IrVhHw@cluster0.ub1rj5g.mongodb.net/?retryWrites=true&w=majority');
 
 //acquire the connection (to check if it is successful)
 const db = mongoose.connection;
